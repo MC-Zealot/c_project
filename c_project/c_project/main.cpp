823b0d169bbbfff79d51c62830a0ada7c3421f6a
Zealot <yizhou.tao@yinyuetai.com>
2016-11-06 17:27:42 +0800

    jj

---
+++ c_project/c_project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  c_project
+//
+//  Created by Zealot on 2016/11/6.
+//  Copyright © 2016年 Zealot. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
