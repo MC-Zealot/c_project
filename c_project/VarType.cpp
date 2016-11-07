7d012abd5cef952f786c70f19f7f4c98e88c3bc7
Zealot <yizhou.tao@yinyuetai.com>
2016-11-07 14:22:17 +0800

    var type

--- c_project/VarType.cpp
+++ c_project/VarType.cpp
@@ -7,3 +7,12 @@
 //
 
 #include <stdio.h>
+
+#include "iostream"
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!!\n";
+}
--- c_project/varType/test.cpp
+++ c_project/varType/test.cpp
@@ -11,10 +11,27 @@
 #include "iostream"
 
 using namespace std;
-
+// 变量声明
+extern int a, b;
+extern int c;
+extern float f;
 int main()
 {
     cout << "Hello World!!\n";
-	int a;
-	cout << a << endl;
+	// 变量定义
+	int a, b;
+	int c;
+	float f;
+		   
+	// 实际初始化
+	a = 10;
+	b = 20;
+	c = a + b;
+				    
+	cout << c << endl ;
+	f = 70.0/3.0;
+	cout << f << endl ;
+					   
+	return 0;
 }
+
