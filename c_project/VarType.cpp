0faf89cc521326a238d0983a9a1d86a2b37045ad
Zealot <yizhou.tao@yinyuetai.com>
2016-11-07 14:10:31 +0800

    var type

---
+++ c_project/VarType.cpp
@@ -0,0 +1,9 @@
+//
+//  VarType.cpp
+//  c_project
+//
+//  Created by Zealot on 2016/11/7.
+//  Copyright © 2016年 Zealot. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ c_project/varType/test.cpp
@@ -0,0 +1,20 @@
+//
+//  VarType.cpp
+//  c_project
+//
+//  Created by Zealot on 2016/11/7.
+//  Copyright © 2016年 Zealot. All rights reserved.
+//
+
+#include <stdio.h>
+
+#include "iostream"
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!!\n";
+	int a;
+	cout << a << endl;
+}
