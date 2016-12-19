21a6f71b4e82b8e3684dc60e46304efdc8c0e439
Zealot <yizhou.tao@yinyuetai.com>
2016-12-19 13:55:22 +0800

    涨粉预估

---
+++ lab_common_promoteuser_20161019/alg_tool/chenwei/new/fanstarget_ad_algorithm_interface.h
@@ -0,0 +1,221 @@
+#ifndef _FANSTARGET_AD_ALGORITHM_INTERFACE_HEADER_
+#define _FANSTARGET_AD_ALGORITHM_INTERFACE_HEADER_
+
+#include "algorithm_interface.h"
+#include "fanstarget_ad.h"
+#include <pthread.h>
+#include <unistd.h>
+typedef map<string,int> Map;
+const uint32_t MAX_FREQUENCY = 15; //最大看广告数目
+const uint32_t MIN_FREQUENCY = 1;  //最小看广告数目
+const uint64_t FLOW_AMOUNT = 50000;	//5min 全局流量估计
+const double BUY_OCCUPY_MAX_PACKAGE = 0.35f;  //最大包估计值
+const uint16_t ADER_PROFILE_DB_NO = 21;		//lushan 
+const uint16_t MID_INDUSTRY_DB_NO = 23;	//mà
+const uint16_t GROUP_NAME_INDUSTRY_DB_NO = 26;
+const double TIME_WEIGHT[] = { 0.71, 0.79, 1.11, 0.89, 1.16, 1.41, 1.31, 1.19,
+	1.35, 1.12, 1.44, 1.24, 1.37, 1.52, 1.52, 1.75,
+	1.68, 0.74, 0.41, 0.25, 0.2, 0.17, 0.24, 0.43 };
+const int64_t FEATURE_HASH_SIZE = 30000;
+void SplitString(string& src, string& token, vector<string>& sp){
+	string::size_type start, end;
+	start = 0;
+	end = src.find(token);
+	while(string::npos != end){
+		sp.push_back(src.substr(start,end - start));
+		start = end + token.size();
+		end = src.find(token, start);
+	}
+	if(start != src.length())
+		sp.push_back(src.substr(start));
+}
+int GetFeatureHash(const char* feature) {
+  unsigned long long a, hash = 0;
+  for (a = 0; a < strlen(feature); a++) hash = hash * 257 + feature[a];
+  hash = hash % (FEATURE_HASH_SIZE - 101);
+  return hash;
+}
+
+
+typedef struct _lushan{
+	int keysize;
+	char** keystr;
+	map<uint64_t,string> result;
+	DbCompany* p_db_company;
+}LUSHAN;
+
+typedef struct _model_data{
+	char** weights;
+	Map** mapArray;
+	int indexkey;
+	int mapNum,weightsNum;
+}model_data;
+
+typedef struct _model_conf{
+	vector<string> mapkeys;
+	vector<string> keys_vec;
+	int arraykey;
+	int indexkey;
+	string update_f;
+	map<string,int> parameters;
+}model_conf;
+
+template <typename TYPE, void (TYPE::*_RunThread)() >  
+void* _thread_t(void* param)//线程启动函数，声明为模板函数  
+{     
+	TYPE* This = (TYPE*)param;     
+	This->_RunThread();     
+	return NULL;  
+}
+
+
+//声明全局变量 供模型使用
+model_data **model_ptr,*model_copy_ptr;
+pthread_t tid_fans_economy;
+pthread_mutex_t p_lock_fans_economy;
+int fans_economy_model_flg = 1;
+
+class FansTargetAlgorithmInterface : public AlgorithmInterface
+{
+	public:
+		FANS_TAR_AD *fansAdThread[3];
+		VEC_CAND fans_ad_vec_thread;
+		map<int, float> weights;
+		map<string, int> feature_index;
+		vector<string> feature_name_default;
+		vector<string> feature_name_list;
+		int *model_flg;//用于标记本线程是否可以创建线程，以及创建后负责创建内存的释放
+		model_data *model_read_ptr;
+		//model_data
+		FansTargetAlgorithmInterface(DbCompany*& p_db_company, int interface_id):
+			AlgorithmInterface(p_db_company, interface_id){
+				LOG_ERROR("init FansTargetAlgorithmInterface.");
+				time_t now;
+				time(&now);
+				for(int i = 0;i < 3; i ++){
+
+					fansAdThread[i] = new FANS_TAR_AD();
+					fansAdThread[i]->type = 1;
+					fansAdThread[i]->field_id = "0";
+					fansAdThread[i]->bridge_uid = 0;
+					fansAdThread[i]->buy_amount = 1000;
+					fansAdThread[i]->max_package = 2000;
+					fansAdThread[i]->finish_amount = 500;
+					fansAdThread[i]->begin_time = now;
+					fansAdThread[i]->ad_type = 2;
+					fansAdThread[i]->brg_type = 1;
+					fansAdThread[i]->alg_log = "";
+					fans_ad_vec_thread.push_back(fansAdThread[i]);
+				}
+//			model_copy_ptr =(model_data *)malloc(sizeof(model_data));	
+			//加载模型
+			//
+			model_flg = new int();
+			//加锁 改变创建线程的全局变量  只创建一个线程 设置允许该线程创建线程 
+			pthread_mutex_init(&p_lock_fans_economy,NULL);
+			pthread_mutex_lock(&p_lock_fans_economy);
+			if (fans_economy_model_flg ==1 ) {
+				fans_economy_model_flg = 0;
+				*model_flg = 1;
+			}
+			pthread_mutex_unlock(&p_lock_fans_economy);
+			int model_num =2;
+			//创建模型更新线程
+			if( *model_flg == 1){
+				//申请初始模型内存，以免释放内存错误
+				model_ptr = new model_data*[model_num]();
+				for (int i=0;i<model_num;i++){
+					model_ptr[i] = new model_data();
+					model_ptr[i]->mapNum = 2;
+					model_ptr[i]->weightsNum=100;
+					model_ptr[i]->mapArray = new Map*[model_ptr[i]->mapNum]();
+					for(int j=0;j<model_ptr[i]->mapNum;j++){
+						model_ptr[i]->mapArray[j] = new Map();
+					}
+					char** m_weights = new char*[model_ptr[i]->weightsNum];
+					for (uint32_t j = 0; j < model_ptr[i]->weightsNum; j++){
+						m_weights[j] = new char[256]();
+					}
+					model_ptr[i]->weights = m_weights;
+				}
+				//创建模型线程
+				pthread_create(&tid_fans_economy,NULL,_thread_t<FansTargetAlgorithmInterface, &FansTargetAlgorithmInterface::_RunThread>, this);
+				LOG_ERROR("init FansTargetAlgorithmInterface.model_flg:%d, pthread_create %d",model_flg,tid_fans_economy);
+			}
+			
+		    	// 特征名字加载，为了交叉，以及不可能所有特征都从引擎带过来，因此存值
+			string INDUSTRY_FEATURE = "f,o,g,p";
+			string INDUSTRY_FEATURE_DEFAULT = "f,o,g1,p2";
+			string token = ",";
+			SplitString(INDUSTRY_FEATURE, token,feature_name_list);
+			SplitString(INDUSTRY_FEATURE_DEFAULT, token, feature_name_default);
+			LOG_ERROR("init success");
+		}
+		~FansTargetAlgorithmInterface() {
+			LOG_ERROR("destroy FansTargetAlgorithmInterface.");
+			for(int i = 0;i < 3; i ++){
+				delete(fansAdThread[i]);
+			}
+			//该线程创建的模型，负责释放模型内存
+			if (*model_flg == 1){
+				pthread_mutex_destroy(&p_lock_fans_economy);
+				for(int i=0;i<2;i++){
+					if (model_ptr[i] != NULL){
+						if(model_ptr[i]->weights != NULL){
+							for(uint16_t j=0;j<model_ptr[i]->weightsNum;++j){
+								delete[] model_ptr[i]->weights[j];
+							}
+							if (model_ptr[i]->weights != NULL)
+								delete model_ptr[i]->weights;
+						}
+						if(model_ptr[i]->mapArray != NULL){
+							for(uint16_t j=0;j<model_ptr[i]->mapNum;++j){
+								delete model_ptr[i]->mapArray[j];
+							}
+							if (model_ptr[i]->mapArray != NULL)
+								delete model_ptr[i]->mapArray;
+						}
+						delete model_ptr[i];
+						if ( model_ptr[i] != NULL ) model_ptr[i] = NULL;
+					}
+				}
+			}
+			delete model_flg;
+			
+		}
+	public:
+		int algorithm_core(uint64_t req_id,const AccessStr& access_str, VEC_CAND& vec_cand);
+		int algorithm_core_new(uint64_t req_id, const AccessInfo* access_info, const VEC_CAND& input_vec, VEC_CAND& output_vec);
+		int slow_order(VEC_CAND& cand_ids,VEC_CAND& output_vec);
+		double get_random();
+		int supply_order(uint16_t num,uint32_t flag,VEC_CAND& input_vec,VEC_CAND& output_vec);//
+		int algorithm_log(VEC_CAND& output_vec,uint16_t return_type,uint16_t num,uint16_t startegy);
+		int so_consume_time(struct timeval& start);	//Í
+		int	log_print(ACCESS_INFO* ai, VEC_CAND& output_vec, uint16_t return_type); //Êä³ö´òÓ¡ÈÕÖ¾
+		int high_quality_user(uint64_t uid, VEC_CAND& input_vec, VEC_CAND& output_vec);
+		int relation_replace(VEC_CAND& input_vec,VEC_CAND& output_vec,ACCESS_INFO* ai);
+		float search_weights(string feature_name, string value,model_data *read_ptr,int value_type);
+	//	void* get_lushan(void* data); //多线程取lushan 数据
+		//float cross_feature(string& left, string& right, string& left_value, string& right_value);
+		int industry_ctr_match(ACCESS_INFO* ai,VEC_CAND& input_vec,VEC_CAND& output_vec, int num);
+		int ctr_match_multi(ACCESS_INFO* ai,VEC_CAND& input_vec,VEC_CAND& output_vec, int num);
+		int industry_ctr_match_level1(ACCESS_INFO* ai,VEC_CAND& input_vec,VEC_CAND& output_vec, int num);
+		//int match_order(uint64_t uid, VEC_CAND& input_vec, VEC_CAND::const_iterator& matcher);
+		int high_quality_flowAnalysis(Fans_Tar_Ad *ft);
+		//double theory_impression_cal(Fans_Tar_Ad * ft, int now_hour, double now_little);
+		double theory_impression_cal(Fans_Tar_Ad * ft, time_t now);
+		//int match_order(uint64_t uid, VEC_CAND& input_vec, VEC_CAND::const_iterator& matcher);
+		//static void* update_model_thread(void *sl);
+		//模型更新函数
+		void update_model(model_conf &mcf,model_data** copy_ptr, model_data** ptr,int lsdb_num,int conf_key);
+		//模型参数解析
+		int parse_model_parameters(model_conf &mcf,int db_num,int key); 
+		//定时任务函数
+		void _RunThread();
+		int search_feature_index(string &key,model_data* read_ptr);
+		int search_industry_score(Fans_Tar_Ad* ft,string& score);
+		int industry_ctr_estimate(ACCESS_INFO* ai,VEC_CAND& input_vec,VEC_CAND& output_vec, int num);
+		int industry_score_threshold(Fans_Tar_Ad* ft,int base_score);
+		float feature_cross_weight(int multi_flg,int fname_idx,string feature_val,int idx,vector<string> &default_info,vector<string> &target_info,int default_flg,model_data *read_ptr);
+};
+#endif
---
+++ lab_common_promoteuser_20161019/alg_tool/src/promotefans_algorithm_interface_3.cpp
@@ -0,0 +1,392 @@
+#include "promotefans_algorithm_interface_3.h"
+
+DYN_ALGORITHMS(PromoteFansAlgorithmInterface);
+
+int PromoteFansAlgorithmInterface::algorithm_core(uint64_t req_id, const AccessStr& access_str,
+		VEC_CAND& vec_cand){
+
+	LOG_ERROR("PromoteFansAlgorithmInterface::algorithm_core return 1\n");
+	return 1;
+}
+
+bool dict_compare(pair<int, float> lhs, pair<int, float> rhs){
+        return lhs.second > rhs.second;
+}
+int PromoteFansAlgorithmInterface::algorithm_core_new(uint64_t req_id, const AccessInfo* access_info, const VEC_CAND& input_vec, VEC_CAND& output_vec){
+	ACCESS_INFO* ai = (ACCESS_INFO*) access_info;
+
+	int size = input_vec.size();
+	LOG_ERROR("PromoteFansAlgorithmInterface inputvec.size():%d\n", size);
+	if(size <= 0){
+		return 1;
+	}
+
+	user_ad_history_ctr(ai, input_vec, output_vec, 3);
+	return 1;
+}
+/**
+ *
+ * @param ai 用户个人信息
+ * @param input_vec 广告候选集
+ * @param output_vec 返回数据
+ * @param num 总共需要返回的广告数
+ * @return
+ */
+int PromoteFansAlgorithmInterface::user_ad_history_ctr(const ACCESS_INFO* ai, const VEC_CAND& input_vec,VEC_CAND& output_vec, int num){
+	//1. 取lushan数据
+	LOG_ERROR("In history_ctr");
+	DbInterface* p_insuff_order_interface = p_db_company_->get_db_interface("NONFANS_USER_PROFILE");
+	if(NULL == p_insuff_order_interface){
+		LOG_ERROR("lushan connect error!");
+		return -1;
+	}
+	uint16_t key_size = input_vec.size() + 1;
+	char** keystr = new char*[key_size];
+	for(uint16_t i = 0; i < key_size; i++)
+		keystr[i] = new char[256];
+	int index = 0;
+	float defaultctr = 0.0;
+	//2. get ader
+
+	for(VEC_CAND::const_iterator it = input_vec.begin();
+			it!=input_vec.end(); it++){
+		User_Order_Ad * ft = (User_Order_Ad*)(*it);
+		sprintf(keystr[index++],"%u-%"PRIu64"", 3, ft->uid);//order_id info;
+	}
+
+	sprintf(keystr[index],"%u-1", 3);//默认ctr
+	map<uint64_t, const char*> result;
+	int count = 0;
+	map<int, float> sort_ad;
+	string token = ",";
+	vector<string> user_profile;
+	float score = 0.0;
+	string def;
+	int redis_flag = ((McDbInterface *)p_insuff_order_interface)->mget(1, keystr, key_size, result);
+	if(redis_flag != 1){
+		LOG_ERROR("redis_flag_error");
+		//随机出广告
+		for(VEC_CAND::const_iterator it = input_vec.begin();it!=input_vec.end(); it++){
+			score = getRandomFloat();
+			User_Order_Ad * ft = (User_Order_Ad*)(*it);
+			ft->adscore = floor(score * 100000);
+			sort_ad.insert(make_pair(count, score));
+			LOG_ERROR("AD:%d %s ADER SCORE RANDOM:%lf", count,ft->order_id.c_str(), score);
+			count++;	
+		}
+		goto FREE_MODULE;
+	}
+	LOG_ERROR("EXPECT:%u GET: %u", key_size, result.size());
+	for(map<uint64_t, const char*>::const_iterator iter = result.begin();iter != result.end(); iter++){
+		LOG_ERROR("LUSHAN %lld:%s", iter->first, iter->second);
+	}
+
+	// 直接比拼ader的  ctr
+	if (result.find(1) != result.end()){
+		def = result[1];
+		defaultctr = atof(def.c_str());
+	}
+	else defaultctr = 0.00043;
+
+	for(VEC_CAND::const_iterator it = input_vec.begin();
+			it!=input_vec.end(); it++){
+		User_Order_Ad * ft = (User_Order_Ad*)(*it);
+		if(result.find(ft->uid)!=result.end()){
+			string str = result[ft->uid];
+			score = atof(str.c_str());
+		}else
+			score = defaultctr;
+
+		ft->adscore = floor(score * 100000);
+		sort_ad.insert(make_pair(count, score));
+		LOG_ERROR("AD:%d %s ADER SCORE:%lf", count,ft->order_id.c_str(), score);
+		count++;
+	}
+
+	//释放内存 
+FREE_MODULE:    
+	for(uint16_t i=0; i < key_size; i++)
+		delete[] keystr[i];
+	if(keystr!=NULL)
+		delete[] keystr;
+
+	if (sort_ad.size() >= 1){
+		vector<pair<int, float> > tmp(sort_ad.begin(), sort_ad.end());
+		sort(tmp.begin(), tmp.end(), dict_compare);
+		if (sort_ad.size()<3) num = sort_ad.size();
+		for (int i = 0;i<num;i++){
+			output_vec.push_back(input_vec[tmp[i].first]);
+		}
+		LOG_ERROR("BEST:%lf", tmp[0].second);
+	}       
+	return 1;
+}
+
+
+void PromoteFansAlgorithmInterface::_RunThread(){
+	vector<pair<int,int> > model_vec;
+	vector<pair<int,int> >::iterator iter;
+	//first is lushan db number; second is model conf
+	model_vec.push_back(make_pair(ADER_PROFILE_DB_NO,23));
+	if(model_vec.size() >0 ){
+		model_conf* mcf;
+		mcf = new model_conf[model_vec.size()];
+		// 初始化配置参数
+		for(iter = model_vec.begin(); iter < model_vec.end(); iter++){
+			mcf[iter - model_vec.begin()].update_f = "-1";
+		}
+		while(1){
+			//{db_key:[21-12,21-13];mapkey:[13:0];arraykey:12;indexkey:13;update:11}
+			//{db_key:[21-21,21-22];mapkey:[22:0];arraykey:21;indexkey:22;update:11}
+			for(iter = model_vec.begin(); iter < model_vec.end(); iter++){
+				int flg = parse_model_parameters(mcf[iter - model_vec.begin()],iter->first,iter->second);
+				if(flg == 1){
+					LOG_ERROR("_RunThread() update model dbnum:%d,conf_key:%d Update",iter->first,iter->second);
+					this->update_model(mcf[iter - model_vec.begin()],&model_copy_ptr,&model_ptr[iter - model_vec.begin()],iter->first,iter->second);
+				}
+				else{
+					LOG_ERROR("_RunThread() update model dbnum:%d,conf_key:%d  Not Need Update ",iter->first,iter->second);
+				}
+
+			}
+			sleep(60);//设置间隔时间 测试设置1分钟
+		}
+		delete[] mcf;
+		if (mcf != NULL) mcf = NULL;
+	}
+}
+/**
+ * 模型更新函数
+ * @param mcf 配置文件
+ * @param copy_ptr 备份的模型
+ * @param ptr 最新的模型
+ * @param lsdb_num
+ * @param conf_key 配置文件在lushan库中的key
+ */
+void PromoteFansAlgorithmInterface::update_model(model_conf &mcf,model_data** copy_ptr, model_data** ptr,int lsdb_num,int conf_key){
+	time_t now;
+	time(&now);
+	LOG_ERROR("MODEL UPDATE BEGIN PTHREAD:%d,dbnum:%d,conf_key:%d",tid_fans_economy,lsdb_num,conf_key);
+	//申请模型内存  申请新模型空间struct
+	model_data* m_ptr =  new model_data();
+	if(mcf.parameters.size() != 0) m_ptr->mapNum = mcf.mapkeys.size()+1;
+	else m_ptr->mapNum = mcf.mapkeys.size();
+	LOG_ERROR("mapNum:%d",m_ptr->mapNum);
+	char** m_weights = NULL;
+	m_ptr->weightsNum = 0;
+	Map** map_i = new Map*[m_ptr->mapNum]();
+	for( uint16_t i=0;i<m_ptr->mapNum;i++){
+		map_i[i] = new Map();
+	}
+	//准备获取lushan数据进行解析
+	map<uint64_t, const char*> result;
+	vector<string> tmp_vector;
+	string token = ";";
+	uint16_t model_keysize = 2;
+	if (mcf.keys_vec.size() != 0) model_keysize = mcf.keys_vec.size();
+	char** keystr = new char*[model_keysize];
+	for (uint16_t i = 0;i<model_keysize;++i){
+		keystr[i] = new char[256]();
+		sprintf(keystr[i],"%s",mcf.keys_vec[i].c_str());//model key
+	}
+	//设置模型更新数据量的标记，以检查模型是否更新正确
+	int feature_newdata_num = 0 ,weight_newdata_num = 0,debug = 1;
+	//connect lushan
+	DbInterface* p_insuff_order_interface = p_db_company_->get_db_interface("NONFANS_USER_PROFILE");
+	int redis_flag = 0;
+	if(NULL == p_insuff_order_interface){
+		LOG_ERROR("lushan connect error!");
+	}
+	else redis_flag = ((McDbInterface *)p_insuff_order_interface)->mget(1, keystr, model_keysize, result);
+	if(debug == 1) LOG_ERROR("MODEL UPDATE LUSHAN DATA EXPECT :%u GET: %u", model_keysize, result.size());
+	//解析数组型模型数据
+	if( mcf.arraykey !=-1 && result.find(mcf.arraykey) != result.end()){
+		tmp_vector.clear();
+		string info = result[mcf.arraykey];
+		SplitString(info, token,tmp_vector);
+		//find max array number
+		int max_num = -1;
+		for(size_t i = 0;i<tmp_vector.size();i++){
+			int idx = tmp_vector[i].find(':');
+			int key = atoi(tmp_vector[i].substr(0,idx).c_str());
+			if (key > max_num) max_num = key;
+		}
+		m_ptr->weightsNum = max_num+1;
+		m_weights = new char*[m_ptr->weightsNum];
+		for (int i = 0; i < m_ptr->weightsNum; i++){
+			m_weights[i] = new char[256]();
+		}
+		//if(debug == 1) LOG_ERROR("MODEL UPDATE ARRAY DATA LUSHAN key:%d   value: %s",mcf.arraykey, info.c_str());
+		if(debug == 1) LOG_ERROR("MODEL UPDATE ARRAY DATA LUSHAN key:%d item num:%d ",mcf.arraykey,tmp_vector.size());
+		for(uint32_t i=0;i<tmp_vector.size();++i){
+			int idx = tmp_vector[i].find(':');
+			int key = atoi(tmp_vector[i].substr(0,idx).c_str());
+			//考虑异常安全性，对写入key范围进行检查
+			if( key < 0 || key >= m_ptr->weightsNum )
+				LOG_ERROR("MODEL UPDATE ARRAR DATA ITERM ERROR key:%d value:%f", key,atof(tmp_vector[i].substr(idx+1).c_str()));
+			else{
+				sprintf(m_weights[key],"%f",atof(tmp_vector[i].substr(idx+1).c_str()));
+				weight_newdata_num++;
+			}
+			//if(debug == 1) LOG_ERROR("MODEL UPDATE ARRAR DATA ITERM CHECK key:%d value:%f", key,atof(tmp_vector[i].substr(idx+1).c_str()));
+		}
+	}
+	//lushan数据错误，跳出
+	if(redis_flag != 1){
+		LOG_ERROR("redis_flag_error");
+		goto FREE_MODULE;
+	}
+	//解析MAP型模型数据 特征离散数据
+	for(int mk=0;mk < m_ptr->mapNum;mk++){
+		if (debug ==1) LOG_ERROR("MODEL UPDATE MAP DATA Iter:%d,%d,%d,%d",mk,mcf.parameters.size(),mcf.mapkeys.size(),m_ptr->mapNum);
+		if ( mcf.parameters.size() != 0 && mk == m_ptr->mapNum-1 ){
+			Map::iterator iter;
+			for(iter = mcf.parameters.begin(); iter != mcf.parameters.end();iter++){
+				if (debug ==1) LOG_ERROR("MODEL UPDATE MAP DATA  parameters %s,%d",iter->first.c_str(),iter->second);
+				(map_i[mk])->insert(make_pair(iter->first,iter->second));
+			}
+			break;
+		}
+		tmp_vector.clear();
+		int idx = mcf.mapkeys[mk].find(":");
+		int qkey = atoi(mcf.mapkeys[mk].substr(0,idx).c_str());
+		int m_pos = atoi(mcf.mapkeys[mk].substr(idx+1).c_str());
+		LOG_ERROR("MAP %d,%d %s",qkey,m_pos,mcf.mapkeys[mk].c_str());
+		if(result.find(qkey) != result.end()){
+			string info = result[qkey];
+			SplitString(info, token,tmp_vector);
+			if (debug ==1) LOG_ERROR("MODEL UPDATE MAP LUSHAN key:%d GET: %s", qkey,info.c_str());
+		}
+		if (debug ==1) LOG_ERROR("MODEL UPDATE MAP DATA key:%d item num:%d ",qkey, tmp_vector.size());
+		for(uint32_t i=0;i<tmp_vector.size();++i){
+			idx = tmp_vector[i].find(':');
+			if (idx != -1 && idx >2 && idx+1 <= tmp_vector[i].size()){
+				//if (debug ==1) LOG_ERROR("MODEL UPDATE MAP DATA ITERM CHECK: key:%s, value:%s",tmp_vector[i].substr(0,idx).c_str(),tmp_vector[i].substr(idx+1).c_str());
+				(map_i[m_pos])->insert(make_pair(tmp_vector[i].substr(0,idx).c_str(),atoi(tmp_vector[i].substr(idx+1).c_str())));
+				feature_newdata_num++;
+			}
+		}
+		if(mcf.indexkey == qkey) m_ptr->indexkey = m_pos;
+		if (debug ==1) LOG_ERROR("MODEL UPDATE MAP DATA key:%d map_pos:%d insert num:%d ",qkey,m_pos,feature_newdata_num);
+	}
+	//检查模型新数据量是正确,错误则goto free model
+	if(feature_newdata_num < 100 || weight_newdata_num < 100 ){
+		LOG_ERROR("MODEL UPDATE ERRO FREE MODEL PTHREAD:%d,dbnum:%d,conf_key:%d",tid_fans_economy,lsdb_num,conf_key);
+		redis_flag =0;
+		goto FREE_MODULE;
+	}
+	m_ptr->mapArray = map_i;
+	m_ptr->weights = m_weights;
+	//加锁 更新模型
+	pthread_mutex_lock(&p_lock_fans_economy);
+	LOG_ERROR("addlock update:%d",tid_fans_economy);
+	*copy_ptr = *ptr;
+	*ptr = m_ptr;
+	//解锁
+	pthread_mutex_unlock(&p_lock_fans_economy);
+	LOG_ERROR("releaselock update:%d",tid_fans_economy);
+	sleep(3);
+	//释放原模型内存
+	if((*copy_ptr)->weights != NULL){
+		for(int i=0;i<(*copy_ptr)->weightsNum;++i){
+			if ((*copy_ptr)->weights[i] != NULL)
+				delete[] (*copy_ptr)->weights[i];
+		}
+		if ((*copy_ptr)->weights != NULL)
+			delete (*copy_ptr)->weights;
+	}
+	if((*copy_ptr)->mapArray != NULL){
+		for(uint16_t i=0;i<(*copy_ptr)->mapNum;i++){
+			if ((*copy_ptr)->mapArray[i] != NULL)
+			delete (*copy_ptr)->mapArray[i];
+		}
+		if ((*copy_ptr)->mapArray != NULL)
+			delete (*copy_ptr)->mapArray;
+	}
+	//new 出来的要delete掉
+	delete *copy_ptr;
+	if(debug == 1) LOG_ERROR("MODEL UPDATE FINISHED UPDATE PTHREAD:%d,dbnum:%d,conf_key:%d",tid_fans_economy,lsdb_num,conf_key);
+	// free memery
+FREE_MODULE:
+	//释放临时空间
+	for (uint32_t i = 0; i<model_keysize; ++i){
+		if (keystr[i] != NULL)
+		delete[] keystr[i];
+	}
+	if (keystr != NULL) delete keystr;
+	//模型内存未使用，释放
+	if(redis_flag != 1){
+		if(debug == 1) LOG_ERROR("MODEL UPDATE ERRO FREE MODEL PTHREAD:%d,dbnum:%d,conf_key:%d",tid_fans_economy,lsdb_num,conf_key);
+		if(m_weights != NULL){
+			for(int i=0;i<m_ptr->weightsNum;++i){
+				if (m_weights[i]!=NULL)
+				delete[] m_weights[i];
+			}
+			if(m_weights != NULL) delete m_weights;
+		}
+		if(map_i != NULL){
+			for(int i=0;i<m_ptr->mapNum;++i){
+				if (map_i[i] != NULL)
+				delete map_i[i];
+			}
+			if(map_i != NULL) delete map_i;
+		}
+		delete m_ptr;
+	}
+	if(debug == 1) LOG_ERROR("MODEL UPDATE TASK FINISHED  PTHREAD:%d,dbnum:%d,conf_key:%d",tid_fans_economy,lsdb_num,conf_key);
+}
+
+/**
+ *ctr预估
+ * @param ai 用户数据
+ * @param input_vec 广告候选集
+ * @param output_vec 返回排好序的广告
+ * @param num 需要返回的数量
+ * @return
+ */
+int PromoteFansAlgorithmInterface::user_ad_ctr_estimate(const ACCESS_INFO* ai, const VEC_CAND& input_vec,VEC_CAND& output_vec, int num){
+	return 1;
+}
+
+int get_user_info(const ACCESS_INFO* ai){
+	ai->gender;
+	ai->os;
+	return 1;
+}
+/**
+ *获取广告各项ctr数据
+ * @param input_vec
+ * @return
+ */
+std::vector<Ad_Info> PromoteFansAlgorithmInterface::get_ad_info(const VEC_CAND& input_vec){
+	vector<Ad_Info> res_ad_infos;
+	int dbno = 3;
+	//取lushan数据
+	LOG_ERROR("get ad info");
+	DbInterface* p_insuff_order_interface = p_db_company_->get_db_interface("NONFANS_USER_PROFILE");
+	if(NULL == p_insuff_order_interface){
+		LOG_ERROR("lushan connect error!");
+		return -1;
+	}
+	uint16_t key_size = input_vec.size() + 1;
+	char** keystr = new char*[key_size];
+	for(uint16_t i = 0; i < key_size; i++)
+		keystr[i] = new char[256];
+	int index = 0;
+	//拼接广告id
+
+	for(VEC_CAND::const_iterator it = input_vec.begin(); it!=input_vec.end(); it++){
+		User_Order_Ad * ft = (User_Order_Ad*)(*it);
+		sprintf(keystr[index++],"%u-%"PRIu64"", dbno, ft->uid);//order_id info;
+	}
+
+	sprintf(keystr[index],"%u-1", dbno);//默认ctr
+	map<uint64_t, const char*> result;
+	int redis_flag = ((McDbInterface *)p_insuff_order_interface)->mget(1, keystr, key_size, result);
+	if(redis_flag != 1){
+			LOG_ERROR("redis_flag_error");
+			return res_ad_infos;
+	}
+	//TODO 拼接广告数据到vector中
+	return res_ad_infos;
+}
